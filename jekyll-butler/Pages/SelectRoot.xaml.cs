ffd4df65f040d454cf3086850d7860278c5f3ba6
Ashhar Hasan <hashhar_dev@outlook.com>
2017-01-10 23:05:35 +0530

    Validate jekyll root selected by user

--- jekyll-butler/Pages/SelectRoot.xaml.cs
+++ jekyll-butler/Pages/SelectRoot.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using jekyll_butler.Pages;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Controls;
 
 namespace jekyll_butler
 {
@@ -7,9 +10,31 @@ namespace jekyll_butler
     /// </summary>
     public partial class SelectRoot : Page
     {
+        public string JekyllRoot { get; set; }
+        public bool ValidJekyllRoot { get; private set; }
+
         public SelectRoot()
         {
             InitializeComponent();
+            SelectRootGrid.DataContext = this;
+        }
+
+        private void LoadJekyllRoot_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var files = Directory.GetFiles(JekyllRoot);
+            ValidJekyllRoot = false;
+            foreach (var file in files)
+            {
+                if (file.EndsWith(@"_config.yml"))
+                {
+                    ValidJekyllRoot = true;
+                    break;
+                }
+            }
+            if (ValidJekyllRoot)
+            {
+                NavigationService.Navigate(new JekyllButlerRoot());
+            }
         }
     }
 }
